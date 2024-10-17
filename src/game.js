722a1b806903231b33ac13c3e6c6c53a39d3146e
Kateryna-1 <katerynasalata@icloud.com>
2024-10-17 20:18:24 +0200

    change css and img

--- src/game.js
+++ src/game.js
@@ -8,7 +8,7 @@ class Game {
     this.scoreTextElement = document.getElementById("score-text");
     this.livesTextElement = document.getElementById("lives-text");
     this.timerElement = document.getElementById("timeRemaining");
-    this.player = new Player(this.gameScreen, 700, 690, 140, 150, "../img/pipka.webp");
+    this.player = new Player(this.gameScreen, 700, 690, 140, 150, "../img/pipka.png");
     this.height = 800;
     this.width = 800;
     this.obstacles = [];
